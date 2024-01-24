8068cda423f96df120145526c7254f319c928682
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-01-24 22:40:04 +0900

    box作業準備（何もしてない

---
+++ box.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Vector2.h>
+#include "intVec2.h"
+
+class Box
+{
+	Vector2 pos;
+	intVec2 posInMapchip;
+	Vector2 velocity;
+	Vector2 acceleraion;
+
+public:
+	Box();
+
+};
