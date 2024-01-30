a34d6e264f00f95fd5b5bea787a808115f78a231
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-01-30 12:26:13 +0900

    シーンマネージャの基盤完成～

---
+++ SceneManager.cpp
@@ -0,0 +1,100 @@
+﻿#include "SceneManager.h"
+
+int		SceneManager::existChangeRequest;		// シーン変更の要求が存在するか
+int		SceneManager::isEndDraw;				// 描画処理が終了したかどうか
+Scenes	SceneManager::scene_current;			// 現在のシーン
+Scenes	SceneManager::scene_next;				// リクエストが来たら代入
+
+int*	SceneManager::title;
+int*	SceneManager::stageSelect;
+int*	SceneManager::game;
+
+void SceneManager::ChangeRequest(Scenes _nextScene)
+{
+	existChangeRequest = 1;
+	scene_next = _nextScene;
+}
+
+void SceneManager::Update()
+{
+	switch (scene_current)
+	{
+	case Title:
+		//if (title) title->Update();
+		break;
+	case StageSelect:
+		//if (stageSelect) stageSelect->Update();
+		break;
+	case Game:
+		//if (game) game->Update();
+		break;
+
+	default:
+		break;
+	}
+}
+
+void SceneManager::Draw()
+{
+	switch (scene_current)
+	{
+	case Title:
+		//if (title) title->Draw();
+		break;
+	case StageSelect:
+		//if (stageSelect) stageSelect->Draw();
+		break;
+	case Game:
+		//if (game) game->Draw();
+		break;
+
+	default:
+		break;
+	}
+}
+
+void SceneManager::ChangeScene()
+{
+	/// シーンチェンジ要求が存在する場合...
+	if (existChangeRequest)
+	{
+		/// 描画が終了しているなら...
+		if (isEndDraw)
+		{
+			// シーンチェンジの演出があれば記述
+			switch (scene_current)
+			{
+			case Title:
+				delete title;
+				title = nullptr;
+				break;
+			case StageSelect:
+				delete stageSelect;
+				stageSelect = nullptr;
+				break;
+			case Game:
+				delete game;
+				game = nullptr;
+				break;
+			default:
+				break;
+			}
+
+			// インスタンスの作成
+			switch (scene_next)
+			{
+			case Title:
+				//title = new Title();
+				break;
+			case StageSelect:
+				//stageSelect = new StageSelect();
+				break;
+			case Game:
+				//game = new Game();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
---
+++ SceneManager.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+enum Scenes
+{
+	Title,
+	StageSelect,
+	Game,
+	// リザルトはゲームシーンに組み込み
+	// 全ステージクリア後はタイトルへ
+};
+
+class SceneManager
+{
+private:
+	static	int		existChangeRequest;		// シーン変更の要求が存在するか
+	static	int		isEndDraw;				// 描画処理が終了したかどうか
+	static	Scenes	scene_current;			// 現在のシーン
+	static	Scenes	scene_next;				// リクエストが来たら代入
+
+	// TODO: 型を変更してください
+	static	int*	title;					// タイトルシーン
+	static	int*	stageSelect;			// ステージセレクトシーン
+	static	int*	game;					// ゲームシーン
+
+public:
+	/// <summary>
+	///	次のフレームからシーンを変更します。
+	/// シーンチェンジの演出がある場合、演出が終わり次第変更します。
+	/// </summary>
+	/// <param name="_nextScene">次のシーン</param>
+	void	ChangeRequest(Scenes _nextScene);
+
+	// 更新処理
+	void	Update();
+	// 描画処理
+	void	Draw();
+	// ゲームループの最後に配置してください。※シーンチェンジ要求はChangeRequest関数を使用して下さい。
+	void	ChangeScene();
+
+};
