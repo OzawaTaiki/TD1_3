17d906bf6681948970fb6d658baa7f100970f7a4
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-02-06 00:57:25 +0900

    バブルクラスの事始め

---
+++ Bubble.cpp
@@ -0,0 +1,17 @@
+﻿#include "Bubble.h"
+
+Bubble::Bubble()
+{
+	sprData.srcPos = Transform(0, 0);
+	sprData.srcSize = Size(1, 1);
+	sprData.textureHandle = ResourceManager::Handle("white");
+}
+
+void Bubble::Update()
+{
+}
+
+void Bubble::Draw()
+{
+
+}
---
+++ Bubble.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "ResourceManager.h"
+#include "UI/UI_ToolKit_Defines.h"
+
+class Bubble
+{
+private:
+	SpriteData	sprData;
+
+public:
+	Bubble();
+
+	void	Update();
+	void	Draw();
+
+};
